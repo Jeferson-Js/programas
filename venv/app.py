5c5a306cd1023998fe67ceab3f977773063adcb4
Jeferson-Js <oliveira.jeferson.jso@gmail.com>
2023-05-04 15:49:05 -0300

    Add the functions in the app how checkbox size and clean cmd.

--- venv/app.py
+++ venv/app.py
@@ -5,8 +5,10 @@ class TelaPython:
         #Tela
         sg.theme('DarkAmber')
         layout = [
-            [sg.Text('Nome'), sg.Input()],
-            [sg.Text('Idade'), sg.Input()],
+            [sg.Text('Nome', size=(5,0)), sg.Input(size=(15,0), key='nome')],
+            [sg.Text('Idade', size=(5,0)), sg.Input(size=(15,0), key='idade')],
+            [sg.Text('Quais provedores de emails são aceitos?')],
+            [sg.Checkbox('Gmail', key='gamil'), sg.Checkbox('Outlook', key='outlook'), sg.Checkbox('Yahool', key='yahool')],
             [sg.Button('Enviar dados')]
         ]
 
@@ -15,7 +17,17 @@ class TelaPython:
         self.button, self.values = window.Read()
 
     def Iniciar(self):
-        print(self.values)
+        nome = self.values['nome']
+        idade = self.values['idade']
+        aceita_gamil = self.values['gamil']
+        aceita_outlook = self.values['outlook']
+        aceita_yahool = self.values['yahool']
+        print(f'Nome: {nome}')
+        print(f'Idade: {idade}')
+        print(f'Aceita gmail: {aceita_gamil}')
+        print(f'Aceita outlook: {aceita_outlook}')
+        print(f'Aceita yahool: {aceita_yahool}')
+        
 
 tela = TelaPython()
 tela.Iniciar()
